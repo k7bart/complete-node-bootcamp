7e07a8e5c9ee6895e11773fe058c6ad4ee2cd95d
Kateryna Bartienieva <k7bart@gmail.com>
2024-11-18 18:26:35 +0200

    add a (very) simple API

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,4 +1,8 @@
+const fs = require("fs");
 const http = require("http");
+
+const data = fs.readFileSync("./dev-data/data.json", "utf-8");
+const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
     const pathName = req.url;
@@ -7,6 +11,9 @@ const server = http.createServer((req, res) => {
         res.end("This is the overview");
     } else if (pathName === "/product") {
         res.end("This is the product");
+    } else if (pathName === "/api") {
+        res.writeHead(200, { "Content-type": "application/json" });
+        res.end(data);
     } else {
         res.writeHead(404, {
             "Content-type": "text/html",
