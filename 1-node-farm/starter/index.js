016f473cbd9d6d7c8e57949dc716510467af3ea9
Kateryna Bartienieva <k7bart@gmail.com>
2024-11-25 16:31:53 +0200

    fill overview and product templates

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,27 +1,54 @@
 const fs = require("fs");
 const http = require("http");
+const url = require("url");
+const replaceTemplate = require("../final/modules/replaceTemplate");
 
+const tempOverview = fs.readFileSync(
+  "./templates/template-overview.html",
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
 const data = fs.readFileSync("./dev-data/data.json", "utf-8");
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-    const pathName = req.url;
+  // const pathName = req.url;
+  const { query, pathname } = url.parse(req.url, true);
 
-    if (pathName === "/" || pathName === "/overview") {
-        res.end("This is the overview");
-    } else if (pathName === "/product") {
-        res.end("This is the product");
-    } else if (pathName === "/api") {
-        res.writeHead(200, { "Content-type": "application/json" });
-        res.end(data);
-    } else {
-        res.writeHead(404, {
-            "Content-type": "text/html",
-        });
-        res.end("<h1>This page could not be found</h1>");
-    }
+  if (pathname === "/" || pathname === "/overview") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+    res.end(output);
+
+    // Product page
+  } else if (pathname === "/product") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
+
+    // API
+  } else if (pathname === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+    });
+    res.end("<h1>This page could not be found</h1>");
+  }
 });
 
 server.listen(8000, "127.0.0.1", () => {
-    console.log("Listening to requests on port 8000");
+  console.log("Listening to requests on port 8000");
 });
